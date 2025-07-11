07a2cc3449b15529fe59ab4a23b200532349d60a
yevhen-petrunkin <novamovaxxi@gmail.com>
2025-07-11 19:14:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css';
+import arrayOne from './arrays/array-1';
+
+const App = () => {
+    return (
+        <div className="w-[2032px] h-[3032px] p-4 mx-auto">
+            <div className="h-[3000px] shadow-2xl w-full bg-amber-950 p-4 relative ">
+                <h1 className="text-8xl h-24 text-amber-50 font-bold uppercase leading-30 text-center">
+                    замкова абетка
+                </h1>
+                <div className="w-full h-[calc(3000px-160px)] mt-8 gap-4 grid grid-cols-4 grid-rows-7">
+                    {arrayOne.map(
+                        ({
+                            number,
+                            letter,
+                            name,
+                            translation,
+                            location,
+                            url,
+                        }) => (
+                            <article
+                                key={number}
+                                className="shadow-2xl relative"
+                            >
+                                <h2 className="absolute z-1 flex justify-center items-center text-amber-950 font-bold size-22 bg-amber-50 text-7xl shadow-2xl leading-8">
+                                    {letter}
+                                </h2>
+                                <div className="absolute inset-0 ">
+                                    <img
+                                        src={url}
+                                        alt={translation}
+                                        className="size-full object-cover"
+                                    />
+                                </div>
+                                <div className="absolute bottom-0 right-0 bg-amber-950/50 shadow-2xl z-1 text-3xl p-2 drop-shadow-2xl text-right text-amber-50">
+                                    <p>{name}</p>
+                                    <p>{`${translation}, ${location}`}</p>
+                                </div>
+                            </article>
+                        ),
+                    )}
+                </div>
+                <p className="absolute bottom-8 right-8 text-amber-50/20 text-8xl">
+                    Skjold Skefsson
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/arrays/array-1.js
@@ -0,0 +1,239 @@
+import castleOneA from '../assets/array-1/castle-01.jpg';
+import castleTwoA from '../assets/array-1/castle-02.jpg';
+import castleThreeA from '../assets/array-1/castle-03.jpg';
+import castleFourA from '../assets/array-1/castle-04.jpg';
+import castleFiveA from '../assets/array-1/castle-05.jpg';
+import castleSixA from '../assets/array-1/castle-06.jpg';
+import castleSevenA from '../assets/array-1/castle-07.jpg';
+import castleEightA from '../assets/array-1/castle-08.jpg';
+import castleNineA from '../assets/array-1/castle-09.jpg';
+import castleTenA from '../assets/array-1/castle-10.jpg';
+import castleElevenA from '../assets/array-1/castle-11.jpg';
+import castleTwelveA from '../assets/array-1/castle-12.jpg';
+import castleThirteenA from '../assets/array-1/castle-13.jpg';
+import castleFourteenA from '../assets/array-1/castle-14.jpg';
+import castleFifteenA from '../assets/array-1/castle-15.jpg';
+import castleSixteenA from '../assets/array-1/castle-16.jpg';
+import castleSeventeenA from '../assets/array-1/castle-17.jpg';
+import castleEighteenA from '../assets/array-1/castle-18.jpg';
+import castleNineteenA from '../assets/array-1/castle-19.jpg';
+import castleTwentyA from '../assets/array-1/castle-20.jpg';
+import castleTwentyOneA from '../assets/array-1/castle-21.jpg';
+import castleTwentyTwoA from '../assets/array-1/castle-22.jpg';
+import castleTwentyThreeA from '../assets/array-1/castle-23.jpg';
+import castleTwentyFourA from '../assets/array-1/castle-24.jpg';
+import castleTwentyFiveA from '../assets/array-1/castle-25.jpg';
+import castleTwentySixA from '../assets/array-1/castle-26.jpg';
+
+const arrayOne = [
+    {
+        number: 1,
+        letter: 'A',
+        name: 'Alcázar of Segovia',
+        translation: 'Алькасар у Сеговії',
+        location: 'Іспанія',
+        url: castleOneA,
+    },
+    {
+        number: 2,
+        letter: 'B',
+        name: 'Bodiam Castle',
+        translation: 'Замок Бодіам',
+        location: 'Англія',
+        url: castleTwoA,
+    },
+    {
+        number: 3,
+        letter: 'C',
+        name: 'Chillon, Château de',
+        translation: 'Шийонський замок',
+        location: 'Швейцарія',
+        url: castleThreeA,
+    },
+    {
+        number: 4,
+        letter: 'D',
+        name: 'Dover Castle',
+        translation: 'Дуврський замок',
+        location: 'Англія',
+        url: castleFourA,
+    },
+    {
+        number: 5,
+        letter: 'E',
+        name: 'Eltz Castle',
+        translation: 'Замок Ельц',
+        location: 'Німеччина',
+        url: castleFiveA,
+    },
+    {
+        number: 6,
+        letter: 'F',
+        name: 'Falaise, Château de',
+        translation: 'Замок Фалез',
+        location: 'Франція',
+        url: castleSixA,
+    },
+    {
+        number: 7,
+        letter: 'G',
+        name: 'Gravensteen',
+        translation: 'Ґравенстен',
+        location: 'Бельгія',
+        url: castleSevenA,
+    },
+    {
+        number: 8,
+        letter: 'H',
+        name: 'Hohenwerfen Castle',
+        translation: 'Замок Гоенверфен',
+        location: 'Австрія',
+        url: castleEightA,
+    },
+    {
+        number: 9,
+        letter: 'I',
+        name: "If, Château d'",
+        translation: 'Замок Іф',
+        location: 'Франція',
+        url: castleNineA,
+    },
+    {
+        number: 10,
+        letter: 'J',
+        name: 'Jánský Vrch',
+        translation: 'Янський Врх',
+        location: 'Чехія',
+        url: castleTenA,
+    },
+    {
+        number: 11,
+        letter: 'K',
+        name: 'Kamianets-Podilskyi Castle',
+        translation: "Кам'янець-Подільська фортеця",
+        location: 'Україна',
+        url: castleElevenA,
+    },
+    {
+        number: 12,
+        letter: 'L',
+        name: 'Leeds Castle',
+        translation: 'Замок Лідс',
+        location: 'Англія',
+        url: castleTwelveA,
+    },
+    {
+        number: 13,
+        letter: 'M',
+        name: 'Malbork Castle',
+        translation: 'Замок Мальборк',
+        location: 'Польща',
+        url: castleThirteenA,
+    },
+    {
+        number: 14,
+        letter: 'N',
+        name: 'Narva Castle',
+        translation: 'Нарвський замок',
+        location: 'Естонія',
+        url: castleFourteenA,
+    },
+    {
+        number: 15,
+        letter: 'O',
+        name: 'Orava Castle',
+        translation: 'Оравський Град',
+        location: 'Словаччина',
+        url: castleFifteenA,
+    },
+    {
+        number: 16,
+        letter: 'P',
+        name: 'Pierrefonds, Château de',
+        translation: "Замок П'єрфон",
+        location: 'Франція',
+        url: castleSixteenA,
+    },
+    {
+        number: 17,
+        letter: 'Q',
+        name: 'Quéribus, Château de',
+        translation: 'Замок Керібюс',
+        location: 'Франція',
+        url: castleSeventeenA,
+    },
+    {
+        number: 18,
+        letter: 'R',
+        name: 'Restormel Castle',
+        translation: 'Замок Рестормел',
+        location: 'Англія',
+        url: castleEighteenA,
+    },
+    {
+        number: 19,
+        letter: 'S',
+        name: 'Stahleck Castle',
+        translation: 'Замок Шталек',
+        location: 'Німеччина',
+        url: castleNineteenA,
+    },
+    {
+        number: 20,
+        letter: 'T',
+        name: 'Trakai Castle',
+        translation: 'Тракайський замок',
+        location: 'Литва',
+        url: castleTwentyA,
+    },
+    {
+        number: 21,
+        letter: 'U',
+        name: 'Uniejów Castle',
+        translation: 'Унеювський замок',
+        location: 'Польща',
+        url: castleTwentyOneA,
+    },
+    {
+        number: 22,
+        letter: 'V',
+        name: 'Vincennes, Château de',
+        translation: 'Венсенський замок',
+        location: 'Франція',
+        url: castleTwentyTwoA,
+    },
+    {
+        number: 23,
+        letter: 'W',
+        name: 'Windsor Castle',
+        translation: 'Віндзорський Замок',
+        location: 'Англія',
+        url: castleTwentyThreeA,
+    },
+    {
+        number: 24,
+        letter: 'X',
+        name: 'Xativa Castle',
+        translation: 'Замок Шатіва',
+        location: 'Іспанія',
+        url: castleTwentyFourA,
+    },
+    {
+        number: 25,
+        letter: 'Y',
+        name: 'York Castle',
+        translation: 'Йоркський замок',
+        location: 'Англія',
+        url: castleTwentyFiveA,
+    },
+    {
+        number: 26,
+        letter: 'Z',
+        name: 'Zvolen Castle',
+        translation: 'Зволенський замок',
+        location: 'Словаччина',
+        url: castleTwentySixA,
+    },
+];
+
+export default arrayOne;
